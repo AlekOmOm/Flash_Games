5743b8c0ba20defe2ca4c55d609068da969185fe
Aleksander Hvid Ellegård <142972647+AlekOmOm@users.noreply.github.com>
2024-10-17 22:19:08 +0200

    init

---
+++ src/main/java/com/alek0m0m/flash_games/FlashGamesApplication.java
@@ -0,0 +1,13 @@
+package com.alek0m0m.flash_games;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlashGamesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlashGamesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/alek0m0m/flash_games/FlashGamesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alek0m0m.flash_games;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashGamesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
